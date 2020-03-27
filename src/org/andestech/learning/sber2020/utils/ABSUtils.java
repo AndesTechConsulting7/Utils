2a7f06f95b5846a534701b9601b605db1c7323cf
Trainer <a0z9@rambler.ru>
2020-03-27 03:39:19 +0000

    Utils v.2 Нормалайзер

--- src/org/andestech/learning/sber2020/utils/ABSUtils.java
+++ src/org/andestech/learning/sber2020/utils/ABSUtils.java
@@ -1,6 +1,25 @@
 package org.andestech.learning.sber2020.utils;
 
+import java.util.Collection;
+import java.util.List;
+
 public class ABSUtils {
+
+    public static void printColl(Collection list)
+    {
+        for (Object o: list) System.out.print(o + ", ");
+        System.out.println("\b\b.");
+    }
+
+    public static void echo(String s, int N)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <N ; i++) sb.append(s);
+        System.out.println(sb.toString());
+
+    }
+
+    public static void echo() { echo("-", 70);}
 
     public static String normalizeName(String n){
         n = n.trim();
